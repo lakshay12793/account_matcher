f8e1805ec196eb287fdf29c32c88511d30207da7
Lakshay <144482440+lakshay12793@users.noreply.github.com>
2025-02-09 22:22:33 +0530

    Add Files via Upload

---
+++ src/main/java/com/bitespeed/IR/model/Contact.java
@@ -0,0 +1,93 @@
+package com.bitespeed.IR.model;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "contacts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String phoneNumber;
+    private String email;
+
+    private Long linkedId; // Refers to primary contact ID
+    private String linkPrecedence; // "primary" or "secondary"
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getLinkedId() {
+        return linkedId;
+    }
+
+    public void setLinkedId(Long linkedId) {
+        this.linkedId = linkedId;
+    }
+
+    public String getLinkPrecedence() {
+        return linkPrecedence;
+    }
+
+    public void setLinkPrecedence(String linkPrecedence) {
+        this.linkPrecedence = linkPrecedence;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public LocalDateTime getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(LocalDateTime deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+    private LocalDateTime deletedAt;
+}
