194bb263cbb6dbf0e8d829cbbcd4c78ed0f860d9
Lakshay <144482440+lakshay12793@users.noreply.github.com>
2025-02-09 22:24:56 +0530

    Add Files via Upload

---
+++ src/test/java/com/bitespeed/IR/IdentityReconciliationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitespeed.IR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityReconciliationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
